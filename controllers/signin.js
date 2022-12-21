7ccc2e980d8e201452f84f8f7f9f7efecab4b4a0
fawwaz95 <fawwaz_95@hotmail.com>
2022-12-21 16:41:30 -0500

    Reverting the change back to original

--- controllers/signin.js
+++ controllers/signin.js
@@ -2,7 +2,7 @@ const handleSignin = (req, res, db, bcrypt) => {
         const {email, password} = req.body;
 
         if(!email || !password){
-            return res.status(400).json('Incorrect form submission!...please check');
+            return res.status(400).json('Incorrect form submission!');
          }
 
            db.select('email', 'hash')
