a5ad640eddc25909d1652e90244e4ae9497fe6ef
fawwaz95 <fawwaz_95@hotmail.com>
2023-01-04 16:36:49 -0500

    testing database_url

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ const signin = require("./controllers/signin.js");
 const profile = require("./controllers/profile.js");
 const image = require("./controllers/image.js");
 
-const db = process.env.DATABASE_URL || console.log('database url does not exist' + process.env.DATABASE_URL);
+const db = process.env.DATABASE_URL || console.log('database url does not exist');
+
+console.log('Here is the DATABASE_URL -> ' + process.env.DATABASE_URL);
 
 const db2 = knex({
     client: 'pg',
-    connection: {
+    connection: db,
+    /*connection: {
       host : '127.0.0.1',
       port : 5432,
       user : 'postgres',
       password : 'password',
       database : 'smartbrain'
-    }
+    }*/
 });
 
 const app = express();
@@ -27,11 +30,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {db.select('*').from('users').then(allUsers => res.send(allUsers))});
-app.post('/signin', (req,res) => signin.handleSignin(req, res, db, bcrypt), (req,res) => res.send(error));
-app.post('/register',(req, res) => register.handleRegister(req, res, db, bcrypt), (req,res) => res.send(error));
-app.get('/profile/:id', (req,res) => profile.handleProfile(req, res, db));
-app.put('/image', (req,res) => image.handleImage(req,res,db));
+app.get('/', (req, res) => {db2.select('*').from('users').then(allUsers => res.send(allUsers))});
+app.post('/signin', (req,res) => signin.handleSignin(req, res, db2, bcrypt), (req,res) => res.send(error));
+app.post('/register',(req, res) => register.handleRegister(req, res, db2, bcrypt), (req,res) => res.send(error));
+app.get('/profile/:id', (req,res) => profile.handleProfile(req, res, db2));
+app.put('/image', (req,res) => image.handleImage(req,res,db2));
 app.post('/imageurl', (req,res) => image.handleApiCall(req,res));
 
 app.listen(PORT, () =>{
